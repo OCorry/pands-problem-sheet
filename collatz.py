f83b7c4abd64a970da61e7098cabdd1b7219d315
Orla <G00411336@gmit.ie>
2022-02-16 21:54:04 +0000

    Week4-Weekly-Task

--- collatz.py
+++ collatz.py
@@ -5,15 +5,15 @@
 #but if its odd, multiply it by 3 and add 1
 
 #Author: Orla Corry
-number =int(input("enter a number:"))
+number =int(input("enter a number:")) #initialising variable and using input statement so i can input in terminal
 
-while number!=1: 
-    if(number % 2) == 0:
-        number = number/2
+while number!=1: #while loop will continue running as long as the value doesnt equal to 1
+    if(number % 2) == 0: #if value is an even number
+        number = number/2 #then divide the number by 2
         print (number)
      
-    else:
-        number = ((number*3)+1)
+    else: #or else, if the value is an odd number 
+        number = ((number*3)+1) #muliply the value by 3 and then add 1
         print(number)
 
         
