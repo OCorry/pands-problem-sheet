3b201eeca6c55bd5fb5adec49ae413b5d06dce94
Orla <G00411336@gmit.ie>
2022-03-05 15:50:32 +0000

    Week06-Weekly-Task

--- squareRoot.py
+++ squareRoot.py
@@ -3,14 +3,16 @@
 #Not using the built in square root function
 #Using Newton's formula
 #Author: Orla Corry 
-n = float(input("Enter a number:")) #allows user to input a floating point number
 
 def newtonSqrt(n, base): #defining the Newton function 
+    n = float(input("Please enter a positive number:")) #allows user to input a floating point number
+                                                        #defining n inside the function so that it is not a global variable
+    
     approx_root = 0.5 * n
     for i in range(base):
         betterapprox = 0.5 * (approx_root + n/approx_root)
         approx_root = betterapprox
-    
+        
     return round(betterapprox, 1) #exiting the function to allow python to run the rest of the program
                                 #(ie to call the function)  
                                 # Using round here to round the decimal place to one           
