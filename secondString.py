ad1f8b0f23f1505e46ef02f3b31e714b0b9496a5
Orla <G00411336@gmit.ie>
2022-03-29 15:02:32 +0100

    Week03-Weekly-Task

--- secondString.py
+++ secondString.py
@@ -1,14 +1,18 @@
-#Program to input a string and output every second letter in reverse order
+#Program to input a sentence (str) and output every second letter in reverse order
+#Sentence: The quick brown fox jumps over the lazy dog.
 #Author: Orla Corry 
 
-#Using input command to allow me to input the string in the terminal
-originalString =input("Please Enter String:")
+#Read in the variable using input() function so that user can input string in the terminal
+originalSentence =input("Please enter a sentence:")
 
-#Using code [::-2] to print every second string in reverse order
-#Reference: https://www.w3schools.com/python/python_howto_reverse_string.asp
-newString = originalString [::-2]
+#Using code [::-2] to print every second letter in reverse order
+secondString = originalSentence [::-2]
 
-print("The New String is :{}" .format(newString))
+#(secondString = originalSentence [8::-2] # I only did this to check my understanding of the code was correct, 
+#and that the output would start with 8th index, k )
+
+#calling the print() function to output the program 
+print (secondString)
 
 
 
