bd876d4a6367077c8e81a3eaf23fc05ccf8b4855
Orla <G00411336@gmit.ie>
2022-03-30 16:33:06 +0100

    Week04-Weekly-Task

--- collatz.py
+++ collatz.py
@@ -1,20 +1,30 @@
 #program that asks user to input a positive integer 
-#and outputs the successive values of the following calculation
-
-#at each step calcultae the next value by taking the current value and, if its even, multiply by 2
+#and outputs the successive values of the following calculation:
+#at each step, calculate the next value by taking the current value and, if its even, multiply by 2
 #but if its odd, multiply it by 3 and add 1
 
 #Author: Orla Corry
-number =int(input("enter a number:")) #initialising variable and using input statement so i can input in terminal
 
-while number!=1: #while loop will continue running as long as the value doesnt equal to 1
-    if(number % 2) == 0: #if value is an even number
-        number = number/2 #then divide the number by 2
-        print (number)
-     
-    else: #or else, if the value is an odd number 
-        number = ((number*3)+1) #muliply the value by 3 and then add 1
-        print(number)
+            
+number = int(input("Please enter a positive integer:"))
+
+list = [] #the program needs to output a list of the numbers that the block of code creates
+list.append(int(number)) #adding a number to the list each time a number genterated
+                            #Using int() so output will be as an integer
+
+while number != 1: #while the number is not equal to 1
+    if number % 2 == 0: #if the number divides by two with no remainder (ie: an even number)
+        number = number / 2
+        list.append(int(number)) #to add the number to the list 
+    else: #if number is an odd number 
+        number = (3 * number) + 1
+        list.append(int(number)) #to add the number to the list 
+
+#calling the function using print() command 
+print(list)
+
+
+
 
         
 
