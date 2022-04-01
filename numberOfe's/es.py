5e05008b4ab7c52eb16df7b3bebb4de3bed823ec
Orla <G00411336@gmit.ie>
2022-04-01 15:37:10 +0100

    Week07-Weekly-Task

--- numberOfe's/es.py
+++ numberOfe's/es.py
@@ -1,30 +1,32 @@
-#Write a program that reads in a text file and outputs the number of e's it contains
-#I am assuming that the client is looking for the number of lower case e's as it is lowecase in the request and they dont specify
+#Write a program that reads in a text file from the command line and outputs the number of e's it contains
+#I am assuming that the user is looking for the number of lower case e's as it is lowecase in the request and they dont specify
 #Author: Orla Corry 
-
+'''
 #I did this first simply to make sure the file would read before moving on to researching the letter counting part of the task
-#import sys 
-#with open(sys.argv[1], 'rt') as f:
-#data = f.read()  #using f.read() as a variable  data so it is more accessable
-#print (data)
-#https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
+import sys 
+with open(sys.argv[1], 'rt') as f:
+    #data = f.read()  #storing f.read() in a variable called data so it is more accessable
+print (data)
+https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
+'''
 
+import sys #need to import sys module to be able to read in a file from the command line
 
-import sys #need to import sys to be able to read a file from command line
-#https://stackoverflow.com/questions/7439145/i-want-to-read-in-a-file-from-the-command-line-in-python
-filename = sys.argv[1]  #storing this in a variable called filename so it is more accessable
+filename = sys.argv[1]  #using variable filename so it is more accessable
+                        #inputting [1] so that the first argument is taken on the command line 
 
 def letterFrequency(f, letter): #defining the function
     
-    with open(filename, 'rt') as f: #open the file in read text mode 
+    with open(filename, 'rt') as f: #open the file in read text mode as 'f'
  
-    # store content of the file in a variable
-         numberOfEs = f.read() #storing what we are looking for in the file as a variable 
+         data = f.read() #using text as variable for the file 'f' leaving the read() blank so the whole text is read
  
-         return numberOfEs.count(letter) 
+         return data.count(letter) #using count() function to count number of letters 
          
         
-print(letterFrequency('f', 'e')) #dont need to specifically define that it's lower case here as Python is case sensitive anyway 
-#Calling the function to print out the number of lower case e's in the txt file
+print(letterFrequency('f', 'e')) #Calling the function to print out the number of lower case e's in the .txt file
+                                #Dont need to specifically define that it's lower case here as Python is case sensitive anyway 
+                                
+                                
 
-#https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
+
