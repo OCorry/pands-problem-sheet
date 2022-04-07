466411f44afd6abc24d73c82348327be029a307b
Orla <G00411336@gmit.ie>
2022-04-07 20:44:27 +0100

    Task

--- number_of_e's.py
+++ number_of_e's.py
@@ -2,15 +2,15 @@
 #I am assuming that the user is looking for the number of lower case e's as it is lowecase in the request and they dont specify
 #Author: Orla Corry 
 
-'''
+
 #I did this first simply to make sure the file would read before moving on to researching the letter counting part of the task
 import sys 
 with open(sys.argv[1], 'rt') as f:
-    #data = f.read()  #storing f.read() in a variable called data so it is more accessable
+    data = f.read()  #storing f.read() in a variable called data so it is more accessable
 print (data)
-https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
-'''
+#https://www.tutorialspoint.com/How-to-read-a-file-from-command-line-using-Python
 
+'''
 import sys #need to import sys module to be able to read in a file from the command line
 
 filename = sys.argv[1]  #using variable filename so it is more accessable
@@ -27,7 +27,7 @@ def letterFrequency(f, letter): #defining the function
         
 print(letterFrequency('f', 'e')) #Calling the function to print out the number of lower case e's in the .txt file
                                 #Dont need to specifically define that it's lower case here as Python is case sensitive anyway 
-                                
+'''                            
                                 
 
 
